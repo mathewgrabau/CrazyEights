ec35711b0212afbdc1e0a28096ea3e2bbf958947
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-12-28 00:40:28 -0600

    Added the button for initiating play, starting launching the game (new screen added). Needs implementation.

---
+++ app/src/main/java/ca/mathewgrabau/crazyeights/CrazyEight.java
@@ -0,0 +1,14 @@
+package ca.mathewgrabau.crazyeights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrazyEight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crazy_eight);
+    }
+}
--- app/src/main/java/ca/mathewgrabau/crazyeights/SplashScreen.java
+++ app/src/main/java/ca/mathewgrabau/crazyeights/SplashScreen.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class SplashScreen extends View {
     private Bitmap titleGraphic;
+    private Bitmap playButtonUp;
+    private Bitmap playButtonDown;
+    private boolean playButtonPressed;
     private int screenWidth;
     private int screenHeight;
 
@@ -18,6 +22,9 @@ public class SplashScreen extends View {
 
         // Init/load the bitmap (image) for the screen
         titleGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.splash_graphic);
+
+        playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
+        playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
     }
 
     @Override
@@ -33,5 +40,53 @@ public class SplashScreen extends View {
         // Position the graphic into the center of the screen (horizontally)
         int titleLeft = (screenWidth - titleGraphic.getWidth()) / 2;
         canvas.drawBitmap(titleGraphic, titleLeft, 100, null);
+
+        int playButtonLeftPosition = (screenWidth - playButtonUp.getWidth()) / 2;
+        if (playButtonPressed) {
+            canvas.drawBitmap(playButtonDown, playButtonLeftPosition, (int)(screenHeight * 0.5),
+                    null);
+        } else {
+            canvas.drawBitmap(playButtonUp, playButtonLeftPosition, (int)(screenHeight * 0.5),
+                    null);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+
+        int touchX = (int)event.getX();
+        int touchY = (int)event.getY();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                int buttonLeft = (screenWidth - playButtonUp.getWidth()) / 2;
+                int buttonRight = buttonLeft + playButtonUp.getWidth();
+                int buttonTop = (int)(screenHeight * 0.5);
+                int buttonBottom = buttonTop + playButtonUp.getHeight();
+
+                boolean inBounds = touchX > buttonLeft && touchX < buttonRight &&
+                        touchY > buttonTop && touchY < buttonBottom;
+                if (inBounds) {
+                    playButtonPressed = true;
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                break;
+
+            case MotionEvent.ACTION_UP:
+                if (playButtonPressed) {
+                    // Launch to main game screen
+
+                }
+
+                // Remove flag regardless, if no touch event then it can't be pressed at all.
+                playButtonPressed = false;
+                break;
+        }
+
+        invalidate();
+        return true;
     }
 }
