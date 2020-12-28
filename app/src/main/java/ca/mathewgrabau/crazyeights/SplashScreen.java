7144eee12721d99cf84214814e5f4024d0a02169
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-12-28 00:10:41 -0600

    Locking into portrait and centering the title screen.

--- app/src/main/java/ca/mathewgrabau/crazyeights/SplashScreen.java
+++ app/src/main/java/ca/mathewgrabau/crazyeights/SplashScreen.java
@@ -4,14 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 
 public class SplashScreen extends View {
     private Bitmap titleGraphic;
+    private int screenWidth;
+    private int screenHeight;
 
     private String TAG = getContext().getClass().getName();
 
@@ -23,33 +21,17 @@ public class SplashScreen extends View {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        screenWidth = w;
+        screenHeight = h;
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(titleGraphic, 100, 100, null);
+        // Position the graphic into the center of the screen (horizontally)
+        int titleLeft = (screenWidth - titleGraphic.getWidth()) / 2;
+        canvas.drawBitmap(titleGraphic, titleLeft, 100, null);
     }
-
-    /*
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                Log.d(TAG, "Down");
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.d(TAG, "Up");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "Move");
-                cx = (int)event.getX();
-                cy = (int)event.getY();
-                break;
-        }
-
-        // Schedule a repainting (invoke draw method for the View).
-        invalidate();
-        return true;
-    }
-
-     */
 }
